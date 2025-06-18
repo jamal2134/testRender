da859f1ff6edcc760fe4fbe7f84e520f0c424760
jamal2134 <59953786+jamal2134@users.noreply.github.com>
2025-06-18 15:06:31 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+# export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export DATABASE_URL="postgresql://testpostgres_9l0t_user:L4iOitIUvz5KWWEZUPsOSZz9ogM1mzV5@dpg-d19aigndiees73anq0ig-a/testpostgres_9l0t"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
